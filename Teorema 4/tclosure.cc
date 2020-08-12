90488e2a28f1586c820a4d04e9a5b370f2d30ee4
Edgar Andreé Toledo Torres <edgar.andree20@gmail.com>
2020-08-12 17:45:24 -0600

    Complemeto de codigo
    Agergado el compilado y bugs del codigo eliminado

--- Teorema 4/tclosure.cc
+++ Teorema 4/tclosure.cc
@@ -1,6 +1,13 @@
+//Diego Estrada
+//Andree Toledo
+//Juan Fernando De Leon
+
+//Proyecto No. 1 Logica matematica.
+
+
 #include<stdio.h> 
 
-//V es la cantidad de vertices 
+//La V define la cantidad de vertices 
 #define V 4 
   
 
@@ -9,36 +16,38 @@ void printSolution(int reach[][V]);
 
 void transitiveClosure(int graph[][V]) 
 { 
+    
+    
     int reach[V][V], i, j, k; 
   
     for (i = 0; i < V; i++) 
         for (j = 0; j < V; j++) 
             reach[i][j] = graph[i][j]; 
   
-    for (k = 0; k < V; k++) 
-    { 
+    for (k = 0; k < V; k++) { 
+        for (i = 0; i < V; i++) { 
 
-        for (i = 0; i < V; i++) 
-        { 
-
-            for (j = 0; j < V; j++) 
-            { 
+            for (j = 0; j < V; j++) { 
 
                 reach[i][j] = reach[i][j] || (reach[i][k] && reach[k][j]); 
             } 
         } 
     } 
   
-
+  
     printSolution(reach); 
+    
 } 
   
 
-void printSolution(int reach[][V]) 
-{ 
-    printf ("Following matrix is transitive closure of the given graph\n"); 
-    for (int i = 0; i < V; i++) 
-    { 
+void printSolution(int reach[][V]) { 
+    
+    printf ("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"); 
+    printf ("|||La siguiente matriz es el cierre transitivo del gráfico dado|||"); 
+    printf ("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"); 
+    
+    for (int i = 0; i < V; i++) { 
+        
         for (int j = 0; j < V; j++) 
             printf ("%d ", reach[i][j]); 
         printf("\n"); 
@@ -46,13 +55,16 @@ void printSolution(int reach[][V])
 } 
   
 
-int main() 
-{   
+int main() {   
+    
+    //Vertices indicados en la matriz ingresada.
+    
     int graph[V][V] = { {0, 0, 1, 1}, 
                         {1, 0, 0, 0}, 
                         {0, 1, 0, 0}, 
                         {0, 0, 0, 0} 
                       }; 
+  
   
     transitiveClosure(graph); 
     return 0; 
